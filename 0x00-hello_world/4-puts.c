66b83bc4c159772f96070b2f1585d74c26da5d2b
mohamed-oukhlidja <mohamadoukhlidja@gmail.com>
2023-06-16 17:26:58 -0700

    tring to make puts.c works

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - a program that prints exactly Programming is like building a multilingual puzzle
+ *main - a program that prints using puts
 return : 0 
  */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
