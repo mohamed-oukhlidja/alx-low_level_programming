035e7f6dcbbc7a7c4533abe4b7f6b7e881256067
mohamed-oukhlidja <mohamadoukhlidja@gmail.com>
2023-06-16 17:24:07 -0700

    fixed some bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * a C program that prints exactly Programming is like building a multilingual puzzle
+ *main - a program that prints exactly Programming is like building a multilingual puzzle
+return : 0 
  */
 int main(void)
 {
 puts ("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
