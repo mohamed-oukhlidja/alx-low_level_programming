500b8c6bd06c3e4760186261f019e01a7fc97951
mohamed-oukhlidja <mohamadoukhlidja@gmail.com>
2023-07-03 08:47:27 -0700

    task 2 is doen

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
 int main(void)
 {
         int i;
@@ -10,5 +12,5 @@ int main(void)
         i = 98;
         positive_or_negative(i);
 
-return (0);
+        return (0);
 }
